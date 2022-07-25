78c7104f9523656954239670e3ae9d42ac1bc705
alexlescionok <alexlescionok@gmail.com>
2022-07-25 21:53:32 +0100

    added initial player function, renamed image file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pygame
 pygame.init()
 
 ##### SET UP GAME WINDOW
-# Create a screen for our game - set the size for height, width - both in pixels
+# Create a screen for our game - set the size for width, height - both in pixels
 screen = pygame.display.set_mode((800, 600))
 
 # Change title displayed in the game window
@@ -22,18 +22,32 @@ pygame.display.set_icon(icon)
 
 #### END OF GAME WINDOW SETUP
 
+# Player spaceship
+player_img = pygame.image.load("images/player_spaceship.png")
+player_x = 370 # x is width - we're starting this a little less than 400 (half of full width) because we want the image to appear centred, for that, the image's left side/corner will need to be set before 400
+player_y = 480 # y is height
+
+def player():
+    screen.blit(player_img, (player_x, player_y)) # blit() is a method that draws something onto the screen - we want to draw our player spaceship on it. Arguments: blit(image, (x coordinates, y coordinates))
 
 # An event is anything that's happening inside our game screen, e.g. closing the window, moving up/down using your arrow keys
 # We need to keep the game screen window open unless the player closes it
 # This is also going to be our main game loop! So everything will have to go into this loop - specifically the for loop going through game events
 running = True
 while running:
+    # Set the RGB fill
+    screen.fill((0, 0, 0)) #obviously we can set the background to something cooler
+    
     # pygame.event.get() grabs all of the events that are happening in the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # checks if the event type is the player closing the game screen window
             running = False
             print("You've quit the game")
     
-    # Set the RGB fill
-    screen.fill((0, 0, 0)) #obviously we can set the background to something cooler
+    
+
+    #Add player - this needs to be drawn after screen.fill(), otherwise the screen will be filled over the player
+    player()
+
     pygame.display.update() # whenever we want to update/add something new to the game window, we must add pygame.display.update() for the change to appear in our window! - be aware, this change is not immediate!
+
