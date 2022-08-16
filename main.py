4c3953b5f1c0e7fbcda48f2b4cf30853f3f2386c
alexlescionok <alexlescionok@gmail.com>
2022-08-16 22:18:58 +0100

    initial class rewrite

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ bullet: <a href="https://www.flaticon.com/free-icons/bullet" title="bullet icons
 Next steps:
 I think to make restart functionality work, we need to utilise classes - at the very least all the examples that I've seen where people talk about restarting, they use classes - once done we can troubleshoot things further.
 The restart logic here makes complete sense: https://stackoverflow.com/questions/13984066/pygame-restart 
+Classes info: https://python.plainenglish.io/learn-classes-by-making-a-game-in-python-5aedee38fc18
 '''
 
 from dis import dis
@@ -18,66 +19,83 @@ import random
 import math
 from pygame import mixer # lets us handle music/audio in pygame
 
-# Initialise the pygame
-pygame.init()
+class Setup():
+    # assigning values to X and Y variable
+    x_window = 800
+    y_window = 600
+    
+    # Define colours
+    green = (0, 255, 0)
+    blue = (0, 0, 128)
+    red = (255, 0, 0)
 
-##### GAME WINDOW SETUP
-# assigning values to X and Y variable
-x_window = 800
-y_window = 600
+    def __init__(self):
+        # Initialise the pygame
+        pygame.init()
 
-# Create a screen for our game - set the size for width, height - both in pixels
-screen = pygame.display.set_mode((x_window, y_window))
+        # Load in background
+        background = pygame.image.load("images/space_background1.png")
 
-# Load in background
-background = pygame.image.load("images/space_background1.png")
+        # Change title displayed in the game window
+        pygame.display.set_caption("space_invaders")
 
-# Change title displayed in the game window
-pygame.display.set_caption("space_invaders")
+        # Set the game icon
+        icon = pygame.image.load("images/spaceship_icon.png")
+        pygame.display.set_icon(icon) 
 
-# Set the game icon
-icon = pygame.image.load("images/spaceship_icon.png")
-pygame.display.set_icon(icon)
+        # Create a screen for our game - set the size for width, height - both in pixels
+        screen = pygame.display.set_mode((self.x_window, self.y_window))
+        # Set the RGB fill
+        screen.fill((0, 0, 0)) #obviously we can set the background to something cooler
+        # Set background image
+        screen.blit(background, (0, 0))
 
-# SET UP SCOREBOARD
-# Define initial score
-score = 0
+    def scoreboard(self):
+        # Define initial score
+        score = 0
 
-green = (0, 255, 0)
-blue = (0, 0, 128)
-red = (255, 0, 0)
+        # reference: https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
+        
+        # Define the font, set the font size to 32; 'freesansbold' is a free font available in pygame
+        # If you wanted to use a different font, you can download fonts, include that font in the folder sctructure and just reference it.
+        font = pygame.font.Font('freesansbold.ttf', 32)
 
-# reference: https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
-# Define colours
-def scoreboard():
+        # Define the text to be displayed
+        # True so we can display it on the screen
+        # The first colour is for the text, the second colour is for the rectangle to fill around the text
+        text = font.render(f"SCOREBOARD: {score}", True, self.green, self.blue)
 
-    # Define the font, set the font size to 32; 'freesansbold' is a free font available in pygame
-    # If you wanted to use a different font, you can download fonts, include that font in the folder sctructure and just reference it.
-    font = pygame.font.Font('freesansbold.ttf', 32)
+        # Create a screen for our game - set the size for width, height - both in pixels
+        screen = pygame.display.set_mode((self.x_window, self.y_window))
 
-    # Define the text to be displayed
-    # True so we can display it on the screen
-    # The first colour is for the text, the second colour is for the rectangle to fill around the text
-    text = font.render(f"SCOREBOARD: {score}", True, green, blue)
+        screen.blit(text, (10, 10))
 
-    screen.blit(text, (10, 10))
+    # SET UP GAME OVER TEXT
+    def game_over_text(self):
+        font = pygame.font.Font('freesansbold.ttf', 48)
+        text = font.render(f"GAME OVER. FINAL SCORE: {score}", True, self.red, self.blue)
+        # use get_rect and center to position text in the middle
+        position = text.get_rect(center=(self.x_window / 2, self.y_window / 2))
+        screen = pygame.display.set_mode((self.x_window, self.y_window))
+        screen.blit(text, position)
 
-# SET UP GAME OVER TEXT
-def game_over_text():
-    font = pygame.font.Font('freesansbold.ttf', 48)
-    text = font.render(f"GAME OVER. FINAL SCORE: {score}", True, red, blue)
-    # use get_rect and center to position text in the middle
-    position = text.get_rect(center=(x_window / 2, y_window / 2))
-    screen.blit(text, position)
+    # SET UP PLAY AGAIN TEXT
+    def play_again_text(self):
+        font = pygame.font.Font('freesansbold.ttf', 32)
+        text = font.render("Press spacebar to play again", True, self.green)
+        position = text.get_rect(center=(self.x_window / 2, self.y_window / 2 + 100))
+        screen = pygame.display.set_mode((self.x_window, self.y_window))
+        screen.blit(text, position)
 
-# SET UP PLAY AGAIN TEXT
-def play_again_text():
-    font = pygame.font.Font('freesansbold.ttf', 32)
-    text = font.render("Press spacebar to play again", True, green)
-    position = text.get_rect(center=(x_window / 2, y_window / 2 + 100))
-    screen.blit(text, position)
+    #### END OF GAME WINDOW SETUP
 
-#### END OF GAME WINDOW SETUP
+
+
+running = True
+while running:
+    setup = Setup()
+    pygame.display.update() # whenever we want to update/add something new to the game window, we must add pygame.display.update() for the change to appear in our window! - be aware, this change is not immediate!
+
 
 
 #### GLOBALS
@@ -167,11 +185,6 @@ def enemy(x_position, y_position, i):
 #### MAIN FUNCTION ###########################
 
 def main():
-    # Set the RGB fill
-    screen.fill((0, 0, 0)) #obviously we can set the background to something cooler
-
-    # Set background image
-    screen.blit(background, (0, 0))
 
     global player_x
     global player_y
@@ -201,9 +214,11 @@ def main():
                 enemy_y[j] = 2000
                 player_y = 2000
             # show game over text
-            game_over = True
+            
             game_over_text()
             play_again_text()
+            game_over = True
+            break
             
         
         # target the relevant index [i] in the num_of_enemies list - without this, the game won't know which enemy to affect as they all have different x and y coordinates
@@ -263,59 +278,63 @@ def main():
 # An event is anything that's happening inside our game screen, e.g. closing the window, moving up/down using your arrow keys
 # We need to keep the game screen window open unless the player closes it
 # This is also going to be our main game loop! So everything will have to go into this loop - specifically the for loop going through game events
-running = True
-while running:
-    main()
-    
-    # pygame.event.get() grabs all of the events that are happening in the game
-    # This for loop checks for events and reacts to them
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: # checks if the event type is the player closing the game screen window
-            running = False
-            print("You've quit the game")
-        # check if the player has used key to move...
-        # KEYDOWN is pressing on the key // KEYUP is releasing a pressed button
-        if event.type == pygame.KEYDOWN: 
-            # if player presses on the left key, move to the left (for as long as the left key is pressed)
-            if event.key == pygame.K_LEFT:
-                # decrease the value of player_x_change (we can add a negative number to decrease the number, e.g. 5 + -0.1 = 4.9)
-                player_x_change -= player_and_bullet_x_speed
-                bullet_x_change -= player_and_bullet_x_speed
-            # if player presses on the right key, move to the left (for as long as the right key is pressed)
-            if event.key == pygame.K_RIGHT:
-                # increase the value of player_x_change
-                player_x_change += player_and_bullet_x_speed
-                bullet_x_change += player_and_bullet_x_speed
-            # if the player presses on the spacebar AND the bullet_state is "ready", then allow the user to fire!
-            # the bullet_state has to be "ready", because that is what it is set before the bullet is every fired and when the bullet_y has been reset back to 480 (check bullet movement - the state gets reverted back to "ready" after initially being "fire")
-            if event.key == pygame.K_SPACE and bullet_state == "ready":
-                # Add in sound effect for the bullet being fired
-                # mixer.Sound is used for shorter sounds, e.g. sound effects
-                bullet_sound = mixer.Sound("audio/fire_sound.wav")
-                bullet_sound.play() # didn't add -1 as an argument because we don't want the sound to play in a loop
-                
-                # define bullet_x to be the same as player_x - this along with bullet_x being passed as an argument in the bullet movement will ensure that the bullet uses the x_position of when it is fired, rather than follow the player spaceship
-                bullet_x = player_x
-                # trigger the fire_bullet function
-                fire_bullet(bullet_x, bullet_y)
-            if event.key == pygame.K_r and game_over == True:
-                print("PRESSED R")
-                main()
-                
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                # Don't change the value of player_x_change - we don't want the player to move any further once the left/right key is no longer pressed
-                player_x_change = 0
-    
-    
-    # restarting reference: https://www.reddit.com/r/pygame/comments/n8vnn2/how_do_i_make_a_restart_button/
-    # game is suuuuuper slow when if game_over block is in the for event in pygame.event.get(): loop / was previously in the while loop
-        # if game_over == False:
-        #     main()
-        # if game_over == True:
-            # if event.key == pygame.K_r:
-            #     game_over = False
+
+while game_over == False:
+    running = True
+    while running:
+        main()
+        # pygame.event.get() grabs all of the events that are happening in the game
+        # This for loop checks for events and reacts to them
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # checks if the event type is the player closing the game screen window
+                running = False
+                print("You've quit the game")
+            # check if the player has used key to move...
+            # KEYDOWN is pressing on the key // KEYUP is releasing a pressed button
+            if event.type == pygame.KEYDOWN: 
+                # if player presses on the left key, move to the left (for as long as the left key is pressed)
+                if event.key == pygame.K_LEFT:
+                    # decrease the value of player_x_change (we can add a negative number to decrease the number, e.g. 5 + -0.1 = 4.9)
+                    player_x_change -= player_and_bullet_x_speed
+                    bullet_x_change -= player_and_bullet_x_speed
+                # if player presses on the right key, move to the left (for as long as the right key is pressed)
+                if event.key == pygame.K_RIGHT:
+                    # increase the value of player_x_change
+                    player_x_change += player_and_bullet_x_speed
+                    bullet_x_change += player_and_bullet_x_speed
+                # if the player presses on the spacebar AND the bullet_state is "ready", then allow the user to fire!
+                # the bullet_state has to be "ready", because that is what it is set before the bullet is every fired and when the bullet_y has been reset back to 480 (check bullet movement - the state gets reverted back to "ready" after initially being "fire")
+                if event.key == pygame.K_SPACE and bullet_state == "ready":
+                    # Add in sound effect for the bullet being fired
+                    # mixer.Sound is used for shorter sounds, e.g. sound effects
+                    bullet_sound = mixer.Sound("audio/fire_sound.wav")
+                    bullet_sound.play() # didn't add -1 as an argument because we don't want the sound to play in a loop
+                    
+                    # define bullet_x to be the same as player_x - this along with bullet_x being passed as an argument in the bullet movement will ensure that the bullet uses the x_position of when it is fired, rather than follow the player spaceship
+                    bullet_x = player_x
+                    # trigger the fire_bullet function
+                    fire_bullet(bullet_x, bullet_y)
+                if event.key == pygame.K_r and game_over == True:
+                    print("PRESSED R")
+                    game_over = False
+                    main()
+                    
+                    
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    # Don't change the value of player_x_change - we don't want the player to move any further once the left/right key is no longer pressed
+                    player_x_change = 0
+            
+            
+        # restarting reference: https://www.reddit.com/r/pygame/comments/n8vnn2/how_do_i_make_a_restart_button/
+        # game is suuuuuper slow when if game_over block is in the for event in pygame.event.get(): loop / was previously in the while loop
+            # if game_over == False:
             #     main()
+            # if game_over == True:
+            #     if event.key == pygame.K_r:
+            #         game_over = False
+            #         main()
+
+        pygame.display.update() # whenever we want to update/add something new to the game window, we must add pygame.display.update() for the change to appear in our window! - be aware, this change is not immediate!
 
 
-    pygame.display.update() # whenever we want to update/add something new to the game window, we must add pygame.display.update() for the change to appear in our window! - be aware, this change is not immediate!
